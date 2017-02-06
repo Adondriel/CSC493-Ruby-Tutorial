020215d472a8ba06b06632546846299bd5dc1b6e
rn4187@cs.ship.edu <rn4187@cs.ship.edu>
2017-02-06 17:52:55 -0500

    Finish user edit, update, index, and destroy actions

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203235327) do
+ActiveRecord::Schema.define(version: 20170206222251) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
